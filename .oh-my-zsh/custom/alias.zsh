15c64b6bcd433d6a87a5d07bc5b36212719a0e61
Rohan Almeida <rohan.almeida@gmail.com>
2020-09-01 19:10:52 +0530

    typo

--- .oh-my-zsh/custom/alias.zsh
+++ .oh-my-zsh/custom/alias.zsh
@@ -8,6 +8,5 @@ alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 export ERL_AFLAGS="-kernel shell_history enabled"
 export JAVA_HOME=/mnt/opt/jdk1.8.0_241
 export ANDROID_SDK_ROOT=/mnt/opt/android-sdk
-export
-PATH=$PATH:$HOME/bin:$JAVA_HOME/bin:/mnt/opt/gradle-6.1.1/bin:$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/tools/bin:$ANDROID_SDK_ROOT/platform-tools:/mnt/opt/anaconda3/bin:/usr/local/go/bin:$HOME/work/flutter/bin:/mnt/opt/android-studio/bin:/mnt/opt/julia-1.5.1/bin:$HOME/.cargo/bin
+export PATH=$PATH:$HOME/bin:$JAVA_HOME/bin:/mnt/opt/gradle-6.1.1/bin:$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/tools/bin:$ANDROID_SDK_ROOT/platform-tools:/mnt/opt/anaconda3/bin:/usr/local/go/bin:$HOME/work/flutter/bin:/mnt/opt/android-studio/bin:/mnt/opt/julia-1.5.1/bin:$HOME/.cargo/bin
 
