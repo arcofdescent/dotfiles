65b3cc5bff54fd4fa960c75c2a753c271c082285
Rohan Almeida <rohan.almeida@gmail.com>
2021-04-25 10:42:53 +0530

    julia 1.6.1

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,8 +3,8 @@ alias vi=vim
 alias rm="rm -i"
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
-set NPM_TOKEN 5140183b8e6f6c478f323ae7daef25e9d1f7e303
+set NPM_TOKEN c7d36b6165c9a5fefd80b6602bc94391960c4cc2
 set ERL_AFLAGS "-kernel shell_history enabled"
 set DOCKER_HOST "unix://$XDG_RUNTIME_DIR/docker.sock"
-set PATH /opt/julia-1.6.0/bin $PATH
+set PATH /opt/julia-1.6.1/bin $PATH
 
