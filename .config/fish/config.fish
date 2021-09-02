6063fcfde84ce627a17def606418bdb4c0e038f7
Rohan Almeida <rohan.almeida@gmail.com>
2021-09-02 12:25:53 +0530

    julia path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,5 +6,5 @@ alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 set NPM_TOKEN c7d36b6165c9a5fefd80b6602bc94391960c4cc2
 set ERL_AFLAGS "-kernel shell_history enabled"
 set DOCKER_HOST "unix://$XDG_RUNTIME_DIR/docker.sock"
-set PATH /opt/julia-1.6.1/bin $HOME/.cargo/bin /usr/local/go/bin $HOME/go/bin $PATH
+set PATH /opt/julia-1.6.2/bin $HOME/.cargo/bin /usr/local/go/bin $HOME/go/bin $PATH
 
