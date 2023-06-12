1cd2ba5dbdcefb522b325e2e8d22ef466292576f
Rohan Almeida <rohan@deriv.com>
2023-06-13 00:31:09 +0530

    use-package everywhere

--- init.el
+++ init.el
@@ -9,13 +9,12 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-;; Download Evil
-(unless (package-installed-p 'evil)
-(package-install 'evil))
+;; Initialize use-package
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-;; Enable Evil
-(require 'evil)
-(evil-mode 1)
+(require 'use-package)
+(setq use-package-always-ensure t)
 
 ;; UTF-8 support
 (prefer-coding-system       'utf-8)
@@ -35,20 +34,43 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                treemacs-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(use-package which-key
+  :config
+  (which-key-mode))
+
+(use-package evil-commentary
+  :config
+  (evil-commentary-mode))
+
 ;; Ibuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
-
-;; Download which-key
-(unless (package-installed-p 'which-key)
-  (package-install 'which-key))
-
-;; Enable which-key
-(require 'which-key)
-(which-key-mode)
-
-;; Download evil-commentary
-(unless (package-installed-p 'evil-commentary)
-  (package-install 'evil-commentary))
-
-;; Enable evil-commentary
-(evil-commentary-mode)
