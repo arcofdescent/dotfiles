26756a52194ab50492035aeae170f0255c1e280e
Rohan Almeida <rohan.almeida@gmail.com>
2025-12-11 23:24:51 +0530

    platform specific font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,9 +3,25 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+-- Detect the OS
+local target = wezterm.target_triple -- e.g. 'x86_64-apple-darwin', 'x86_64-unknown-linux-gnu'
+
 -- Appearance
-config.font = wezterm.font("JetBrains Mono Nerd Font")
-config.font_size = 10.0
+-- Set platform-specific font
+if target:find('apple') then
+  -- macOS
+  config.font = wezterm.font("JetBrains Mono")
+  config.font_size = 12.0
+elseif target:find('linux') then
+  -- Linux
+  config.font = wezterm.font("JetBrains Mono Nerd Font")
+  config.font_size = 10.0
+else
+  -- Default fallback for other OSes (Windows, etc)
+  config.font = wezterm.font("JetBrains Mono Nerd Font")
+  config.font_size = 10.0
+end
+
 config.line_height = 1.1
 config.color_scheme = "Tokyo Night Moon"
 config.window_decorations = "RESIZE"
